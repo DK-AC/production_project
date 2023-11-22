7707b8141ff187949daf7b5e3e92f362b3a57610
DK-AC <Denkacaj@gmail.com>
2023-11-22 14:04:31 +0300

    add: loader ,pageLoader use:loader

--- src/app/providers/router/AppRouter.tsx
+++ src/app/providers/router/AppRouter.tsx
@@ -2,19 +2,22 @@ import React, { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { routeConfig } from 'shared/config/routeConfig/RouteConfig'
 import { useTranslation } from 'react-i18next'
+import { PageLoader } from 'widgets/pageLoader'
 
 export function AppRouter() {
   const { t } = useTranslation()
 
   return (
-    <Suspense fallback={<div>{t('loading')}</div>}>
-      <div className="page-wrapper">
+    <div className="page-wrapper">
+      <Suspense fallback={<PageLoader />}>
+
         <Routes>
           {routeConfig.map(({ path, element }) => (
             <Route key={path} path={path} element={element} />
           ))}
         </Routes>
-      </div>
-    </Suspense>
+      </Suspense>
+
+    </div>
   )
 }
