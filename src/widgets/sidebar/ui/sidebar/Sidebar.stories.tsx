22952db0b6f65aed45ce8b85c9512a40fb7a3df5
DK-AC <Denkacaj@gmail.com>
2023-11-27 14:30:49 +0300

    add: sidebar stories

---
+++ src/widgets/sidebar/ui/sidebar/Sidebar.stories.tsx
@@ -0,0 +1,24 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+
+import React from 'react'
+import { Theme } from 'app/providers/themeProvider/lib/ThemeContext'
+import { ThemeDecorator } from 'shared/config/storybook/themeDecorator/themeDecorator'
+import { Sidebar } from './Sidebar'
+
+export default {
+  title: 'widget/Sidebar',
+  component: Sidebar,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Sidebar>
+
+const Template: ComponentStory<typeof Sidebar> = (args) => <Sidebar {...args} />
+
+export const Light = Template.bind({})
+Light.args = {}
+Light.decorators = [ThemeDecorator(Theme.LIGHT)]
+
+export const Dark = Template.bind({})
+Dark.args = {}
+Dark.decorators = [ThemeDecorator(Theme.DARK)]
