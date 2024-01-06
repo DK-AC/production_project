b90bf3f4b9e692cc4e6d604a1353f357347b3bfd
DK-AC <Denkacaj@gmail.com>
2024-01-06 17:01:31 +0300

    remove btn test

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,12 +1,12 @@
-import webpack from 'webpack'
+import { ResolveOptions } from 'webpack'
 import { BuildOptions } from './types/config'
 
-export function buildResolvers(options: BuildOptions): webpack.ResolveOptions {
+export function buildResolvers(options: BuildOptions): ResolveOptions {
   return {
     extensions: ['.tsx', '.ts', '.js'],
-    alias: {},
-    mainFiles: ['index'],
-    modules: [options.paths.src, 'node_modules'],
     preferAbsolute: true,
+    modules: [options.paths.src, 'node_modules'],
+    mainFiles: ['index'],
+    alias: {},
   }
 }
