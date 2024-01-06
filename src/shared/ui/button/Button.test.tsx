ba8aec362f45a9867137c219d823a2025c7b59c5
DK-AC <Denkacaj@gmail.com>
2024-01-06 17:18:34 +0300

    retry test

--- src/shared/ui/button/Button.test.tsx
+++ src/shared/ui/button/Button.test.tsx
@@ -1,14 +1,16 @@
+import { ComponentRender } from 'shared/lib/tests'
+import { screen } from '@testing-library/react'
+import { Button, ButtonTheme } from './Button'
+
 describe('Button', () => {
-  test('test', () => {
+  test('Test render', () => {
+    ComponentRender(<Button>TEST</Button>)
+    expect(screen.getByText('TEST')).toBeInTheDocument()
   })
-  // test('Test render', () => {
-  //   render(<Button>TEST</Button>)
-  //   expect(screen.getByText('TEST')).toBeInTheDocument()
-  // })
-  //
-  // test('Test clear theme', () => {
-  //   render(<Button theme={ButtonTheme.CLEAR}>TEST</Button>)
-  //   expect(screen.getByText('TEST')).toHaveClass('clear')
-  //   screen.debug()
-  // })
+
+  test('Test clear theme', () => {
+    ComponentRender(<Button theme={ButtonTheme.CLEAR}>TEST</Button>)
+    expect(screen.getByText('TEST')).toHaveClass('clear')
+    screen.debug()
+  })
 })
